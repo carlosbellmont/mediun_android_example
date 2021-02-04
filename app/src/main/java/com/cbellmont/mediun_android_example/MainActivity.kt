cd916b088acf6d511f4bd18fa3305241131376de
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2021-02-04 15:14:19 +0100

    First Commit

--- app/src/main/java/com/cbellmont/mediun_android_example/MainActivity.kt
+++ app/src/main/java/com/cbellmont/mediun_android_example/MainActivity.kt
@@ -2,10 +2,14 @@ package com.cbellmont.mediun_android_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.cbellmont.mediun_android_example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
